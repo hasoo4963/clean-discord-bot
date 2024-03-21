5228fd7d32212cc2b3e8890bfe3cf5e1b9863d81
Your Name <hasoo4963@gmail.com>
2024-03-21 16:50:13 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,62 @@
+import discord
+import asyncio
+from datetime import datetime
+
+TOKEN = 'MTIxOTkyMjgzNTYyNDAzNDM2NA.G42Anp.KdYfwIFJsmDjfbc9z2CYL1s71BmAdDoPkLh6UI'
+CHANNEL_ID = '1219921240656183309'
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+        await self.change_presence(status=discord.Status.online, activity=discord.Game("대기중"))
+
+    async def on_message(self, message):
+
+        if message.content == "/삭제":
+            await message.channel.purge(limit=2)
+            embd = discord.Embed(title="메시지 삭제 완료 !", description="")
+            embed = await message.channel.send(embed = embd)
+
+            await asyncio.sleep(3)
+            await embed.delete()
+
+    def get_day_of_week(self):
+        weekday_list = ['월요일', '화요일', '수요일', '목요일', '금요일', '토요일', '일요일']
+
+        weekday = weekday_list[datetime.today().weekday()]
+        date = datetime.today().strftime("%Y년 %m월 %d일")
+        result = '{}({})'.format(date, weekday)
+        return result
+
+    def get_time(self):
+        return datetime.today().strftime("%H시 %M분 %S초")
+
+    def get_answer(self, text):
+        trim_text = text.replace(" ", "")
+
+        answer_dict = {
+            '안녕': '안녕하세요. MyBot입니다.',
+            '요일': ':calendar: 오늘은 {}입니다'.format(self.get_day_of_week()),
+            '시간': ':clock9: 현재 시간은 {}입니다.'.format(self.get_time()),
+        }
+
+        if trim_text == '' or None:
+            return "알 수 없는 질의입니다. 답변을 드릴 수 없습니다."
+        elif trim_text in answer_dict.keys():
+            return answer_dict[trim_text]
+        else:
+            for key in answer_dict.keys():
+                if key.find(trim_text) != -1:
+                    return "연관 단어 [" + key + "]에 대한 답변입니다.\n" + answer_dict[key]
+
+            for key in answer_dict.keys():
+                if answer_dict[key].find(text[1:]) != -1:
+                    return "질문과 가장 유사한 질문 [" + key + "]에 대한 답변이에요.\n" + answer_dict[key]
+
+        return text + "은(는) 없는 질문입니다."
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = MyClient(intents=intents)
+client.run(TOKEN)
